ab307d7eb9b2381276f006ca72ec4e8dc19af460
yogain19 <yogendrasaxena56@gmail.com>
2019-10-27 18:56:15 +0530

    golang channels and golang routins

--- personal.go
+++ personal.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
 	"strings"
 )
 
 type hola []int
+
+type Person struct {
+	name string
+	age  int
+}
 
 func main() {
 	var arr = hola{1, 2, 3, 4, 5}
@@ -16,6 +23,48 @@ func main() {
 	var stringArr = strings.Split(res, " ")
 	fmt.Println(len(stringArr))
 	arr.logme("yogendra")
+
+	var p Person
+	p.name = "yogendra"
+	p.age = 25
+	fmt.Println((p))
+	updateName(&p)
+	fmt.Println(p)
+	p.updateAge()
+	fmt.Println(p)
+
+	c := make(chan int) // making channel
+
+	for _, item := range arr {
+		go delay(item, c) // go routin
+		//fmt.Println(<-c)  // receiving data from a channel is a blocking operation
+		// channelValue := <-c
+		// fmt.Println(channelValue)
+	}
+
+	for i := 0; i < len(arr); i++ {
+		fmt.Println(<-c)
+	}
+}
+
+func delay(item int, c chan int) {
+	_, error := http.Get("https://www.google.com")
+	if error != nil {
+		fmt.Println("Error")
+		c <- 404 // putting data in channel
+		os.Exit(1)
+	}
+	//fmt.Println("success")
+	c <- 200 // putting data in channel
+
+}
+
+func updateName(p *Person) {
+	(*p).name = "saxena"
+}
+
+func (p *Person) updateAge() {
+	(*p).age = 35
 }
 
 func (h hola) logme(name string) {
